6cca4fc844d604245cb7e16e23c1869721b2e726
Shurick <shurick2211@gmail.com>
2025-06-07 13:09:12 +0300

    win notify

--- src/main/kotlin/com/nimko/trayapp/services/NotificationWindows.kt
+++ src/main/kotlin/com/nimko/trayapp/services/NotificationWindows.kt
@@ -1,16 +1,38 @@
 package com.nimko.trayapp.services
 
 class NotificationWindows: Noticeable {
-    override fun notification(title: String, message: String) {
-        Runtime.getRuntime().exec(arrayOf("powershell", "-command",
-            "[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] > \$null;" +
-                    "\$template = [Windows.UI.Notifications.ToastNotificationManager]::GetTemplateContent([Windows.UI.Notifications.ToastTemplateType]::ToastText02);" +
-                    "\$textNodes = \$template.GetElementsByTagName('text');" +
-                    "\$textNodes.Item(0).AppendChild(\$template.CreateTextNode('Hello')); " +
-                    "\$textNodes.Item(1).AppendChild(\$template.CreateTextNode('Message from your app.'));" +
-                    "\$toast = [Windows.UI.Notifications.ToastNotification]::new(\$template);" +
-                    "\$notifier = [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier('$title');" +
-                    "\$notifier.Show($message);"))
+//    override fun notification(title: String, message: String) {
+//        Runtime.getRuntime().exec(arrayOf("msg", "*", "$title: $message"))
+//    }
 
-    }
+override fun notification(title: String, message: String) {
+    val command = """
+            Add-Type -AssemblyName System.Windows.Forms
+            [System.Windows.Forms.MessageBox]::Show('$message', '$title', 'OK', 'Information')
+        """.trimIndent()
+
+    Runtime.getRuntime().exec(arrayOf("powershell", "-Command", command))
+}
+
+//    override fun notification(title: String, message: String) {
+//        val command = """
+//            Add-Type -AssemblyName System.Windows.Forms
+//            Add-Type -AssemblyName System.Drawing
+//
+//            ${'$'}notify = New-Object System.Windows.Forms.NotifyIcon
+//            ${'$'}notify.Icon = [System.Drawing.SystemIcons]::Information
+//            ${'$'}notify.Visible = ${'$'}true
+//            ${'$'}notify.ShowBalloonTip(5000, '$title', '$message', [System.Windows.Forms.ToolTipIcon]::Info)
+//            Start-Sleep -Seconds 6
+//            ${'$'}notify.Dispose()
+//        """.trimIndent()
+//
+//        Runtime.getRuntime().exec(arrayOf(
+//            "powershell",
+//            "-WindowStyle", "Hidden",
+//            "-Command",
+//            command
+//        ))
+//    }
+
 }
