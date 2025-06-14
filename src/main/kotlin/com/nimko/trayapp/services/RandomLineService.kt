39c6b6a5dc822b37c3fff6e33ffdadb0e0c59f2d
Shurick <shurick2211@gmail.com>
2025-06-14 15:20:31 +0300

    quote in

--- src/main/kotlin/com/nimko/trayapp/services/RandomLineService.kt
+++ src/main/kotlin/com/nimko/trayapp/services/RandomLineService.kt
@@ -2,6 +2,7 @@ package com.nimko.trayapp.services
 
 import jakarta.annotation.PostConstruct
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.nio.file.Files
 import java.nio.file.Paths
@@ -13,15 +14,14 @@ class RandomLineService {
     private val log = LoggerFactory.getLogger(RandomLineService::class.java)
     private val lines = mutableListOf<String>()
 
-    private val filePath = "./quote.txt"
 
     @PostConstruct
     fun init() {
-        val path = Paths.get(filePath)
-        if (Files.exists(path)) {
-            lines.addAll(Files.readAllLines(path))
+        val inputStream = javaClass.classLoader.getResourceAsStream("quote.txt")
+        if (inputStream != null) {
+            inputStream.bufferedReader().useLines { lines.addAll(it) }
         } else {
-            log.warn("File path $filePath does not exist")
+            log.warn("Resource quote.txt not found in classpath")
         }
     }
 
